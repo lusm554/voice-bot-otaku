6d677ffec93d0397f2a1cb08583e0976633dc787
lusm <loveyousomuch554@gmail.com>
2023-01-21 17:34:47 +0300

    otaku dir

--- otaku/bot.py
+++ otaku/bot.py
@@ -1,13 +1,16 @@
 import discord
+import os
+from dotenv import load_dotenv
 
-token=""
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 class BaseClient(discord.Client):
     async def on_ready(self):
-        print(f"Logged on as {self.user}!")
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
 
     async def on_message(self, msg):
         if msg.author == client.user:
@@ -15,6 +18,7 @@ class BaseClient(discord.Client):
 
         if msg.content.startswith("$hello"):
             await msg.channel.send("hello")
+
 
 client = BaseClient(intents=intents)
 client.run(token)
