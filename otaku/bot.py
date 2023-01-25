4ec1466ab3b4a227da8c5a54ce66ae9c96121e0d
lusm <loveyousomuch554@gmail.com>
2023-01-26 01:47:05 +0300

    added first cog, vcontrol!

--- otaku/bot.py
+++ otaku/bot.py
@@ -1,7 +1,11 @@
 import os
 import exceptions
-import discord
+import asyncio
 from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import (ExtensionFailed, ExtensionNotFound, NoEntryPointError)
 
 try:
     load_dotenv()
@@ -9,11 +13,33 @@ try:
 except KeyError:
     raise exceptions.MissingEnvironmentVariable(f"Environment variable 'DISCORD_TOKEN' does not defined.")
 
-print(DISCORD_TOKEN)
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-#bot.run(DISCORD_TOKEN)
+
+class OtakuBot(commands.Bot):
+    def __init__(self, **args):
+        super().__init__(**args)
+
+    async def on_ready(self) -> None:
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print()
+
+    async def setup_hook(self) -> None:
+        """A coroutine to be called to setup the bot, by default this is blank."""
+        try:
+            await self.load_extension("cogs.vcontrol")
+        except (ExtensionFailed, ExtensionNotFound, NoEntryPointError):
+            print(f"Failed to load extension cogs.vcontrol")
+
+
+bot = OtakuBot(
+    command_prefix="/",    
+    intents=intents
+)
+if __name__ == "__main__":
+    bot.run(DISCORD_TOKEN)
+
 
---
+++ otaku/cogs/vcontrol.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+
+class Vcontrol(commands.Cog, name="vcontrol"):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def vctl(self, ctx):
+        await ctx.send(f"Hello")
+
+async def setup(bot) -> None:
+    await bot.add_cog(Vcontrol(bot))
+
