c68e83863988c56096c218fda7423cd3b330787f
lusm <loveyousomuch554@gmail.com>
2023-02-23 23:16:03 +0300

    added time zone to log msg

--- otaku/log.py
+++ otaku/log.py
@@ -1,12 +1,11 @@
 import logging
 
-
 def get_logger(name: str = None):
     """ Generate logger by name. """
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter(
-        "%(asctime)s %(name)s[%(lineno)d] - %(levelname)s: %(message)s", datefmt="%m/%d/%y %H:%M:%S"
+        f"%(asctime)s %(name)s[%(lineno)d] - %(levelname)s: %(message)s", datefmt="%m-%d-%Y %H:%M:%S %Z%z"
     )
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
