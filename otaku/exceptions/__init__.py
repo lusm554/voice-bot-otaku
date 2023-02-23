99a420638108b9499daf3cc86e9ea16ab27b1afd
lusm <loveyousomuch554@gmail.com>
2023-02-24 01:10:27 +0300

    new exception for type casts error

--- otaku/exceptions/__init__.py
+++ otaku/exceptions/__init__.py
@@ -1,3 +1,7 @@
 class MissingEnvironmentVariable(Exception):
     """Describes error of missing environment variable.""" 
     ...
+
+class UnableCastLiteral(Exception):
+    """Describes a type conversion error that is not possible."""
+    ...
